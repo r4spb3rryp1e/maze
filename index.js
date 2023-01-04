1d1c2e351d07ee3995820144418ba98a01e23cc9
r4spb3rryp1e <r4spb3rryp1e@karan.dev>
2023-01-04 18:12:05 -0500

    New Commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Engine, Render, Runner, World, Bodies } = Matter; 
+const { Engine, Render, Runner, World, Bodies,
+    MouseConstraint,
+    Mouse
+
+} = Matter; 
 
 const engine = Engine.create();
 const { world } = engine;
@@ -14,9 +18,18 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}))
 
-const shape = Bodies.rectangle(200,200, 50, 50, {
-    isStatic: true
-});
+//Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
+    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
+    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
+    Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
+];
 
-World.add(world, shape)
+World.add(world, walls)
+
+World.add(world, Bodies.rectangle(200,200, 50, 50,));
